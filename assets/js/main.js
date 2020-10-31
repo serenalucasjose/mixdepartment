62a02e4c501c6f3fc8d57e42bbe7791077abbb87
Lucas Jose Serena <serenalucasjose@gmail.com>
2020-10-31 15:53:36 -0300

    minor fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ $(function() {
     
     $(window).on('load', function(event) {
       setLang('en');
-      $('.preloader').delay(100).fadeOut(100);
+      $('.preloader').fadeOut(100);
     });
     
     //===== Sticky
